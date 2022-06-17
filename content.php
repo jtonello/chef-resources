45578773e7d3a3eedc0cd6e388893f32061ef3fd
John S. Tonello <21206201+jtonello@users.noreply.github.com>
2022-06-16 22:11:43 -0400

    Create content.php

---
+++ content.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <base href="https://docs.chef.io/" target="_blank">
+</head>
+<body>
+<?php
+echo $myhref = $_GET['myhref'];
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+$response = WpOrg\Requests\Requests::get($myhref);
+var_dump($response->body);
+
+?>
+</body>
+</html>
